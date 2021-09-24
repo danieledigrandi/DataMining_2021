f2c4cc43d69c48650d93010a9bb8779c1188e925
Alice <alice.hartog@live.it>
2021-09-24 15:34:45 +0200

    Importing files

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -1,10 +1,15 @@
 # Data mining project 2021 - assignment 1
+import pandas as pd
+import numpy
 
+# importing credit file
+credit = pd.read_csv('.\data\credit.txt')
 
+# importing pima file
+pima = pd.read_csv('.\data\pima.txt')
 
+# importing eclipse for training
+training = pd.read_csv('.\data\promise-2_0a-packages-csv\eclipse-metrics-packages-2.0.csv', sep = ';')
 
-
-
-
-
-
+# importing eclipse for testing
+testing = pd.read_csv('.\data\promise-2_0a-packages-csv\eclipse-metrics-packages-3.0.csv', sep = ';')
